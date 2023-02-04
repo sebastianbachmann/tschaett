1ffa5e66e723ca1c9113e8f4b9274ce93a834df0
Sebastian Bachmann <github@sebastianbachmann.de>
2023-02-04 17:06:49 +0100

    feat: Added Pub/Sub to Broadcast over the Topics

--- lib/chat_web/live/topic_live.ex
+++ lib/chat_web/live/topic_live.ex
@@ -6,6 +6,10 @@ defmodule ChatWeb.TopicLive do
   def mount(params = %{"topic_name" => topic_name}, _session, socket) do
     Logger.info(params: params)
 
+    if connected?(socket) do
+      ChatWeb.Endpoint.subscribe(topic_name)
+    end
+
     username = AnonymousNameGenerator.generate_random()
 
     {:ok, assign(socket, topic_name: topic_name, username: username, message: "")}
@@ -14,10 +18,17 @@ defmodule ChatWeb.TopicLive do
   def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
     Logger.info(submit_message: message)
 
+    ChatWeb.Endpoint.broadcast(socket.assigns.topic_name, "new_message", message)
+
     {:noreply, assign(socket, message: "")}
   end
 
   def handle_event("message_change", %{"chat" => %{"message" => message}}, socket) do
     {:noreply, assign(socket, message: message)}
+  end
+
+  def handle_info(%{event: "new_message", payload: message_data}, socket) do
+    Logger.info(message_data: message_data)
+    {:noreply, socket}
   end
 end
